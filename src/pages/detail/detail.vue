6a54a7ed23c4c866d70924789c95691cb06215d9
codermyj <bytjutmyj@163.com>
2019-04-16 13:58:25 +0800

    增加图书详情页面

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,6 +2,20 @@
     <div>
       <BookInfo :info="bookInfo"></BookInfo>
       <textarea v-model="comment" class="textarea" placeholder="请输入图书评论" :maxlength="100"></textarea>
+      <div class="location">
+        地理位置:
+        <switch color="2d8cf0" :checked="location" @change="getGeo" />
+        <span class="text-primary">
+          {{location}}
+        </span>
+      </div>
+      <div class="phone">
+        手机型号:
+        <switch color="2d8cf0" :checked="phone" @change="getPhone" />
+        <span class="text-primary">
+          {{phone}}
+        </span>
+      </div>
     </div>
 </template>
 
@@ -16,7 +30,9 @@ export default {
   data () {
     return {
       bookid: '',
-      bookInfo: {}
+      bookInfo: {},
+      location: '',
+      phone: ''
     }
   },
   methods: {
@@ -28,6 +44,45 @@ export default {
       })
       this.bookInfo = bookdetail.data
       this.bookInfo.detail = bookdetail.data.title.repeat(50)
+    },
+    getGeo (e) {
+      const ak = '9ugdZD44BniceIzbPpiGlorKVK1qa24L'
+      let url = 'http://api.map.baidu.com/geocoder/v2/'
+      if (e.target.value) {
+        wx.getLocation({
+          success: geo => {
+            console.log(geo)
+            wx.request({
+              url,
+              data: {
+                ak,
+                location: `${geo.latitude},${geo.longitude}`,
+                output: 'json'
+              },
+              success: res => {
+                console.log(res)
+                if (res.data.status === 0) {
+                  this.location = res.data.result.formatted_address
+                } else {
+                  this.location = '未知地点'
+                }
+              }
+            })
+          }
+        })
+      } else {
+        this.location = ''
+      }
+    },
+    getPhone (e) {
+      if (e.target.value) {
+        const phoneInfo = wx.getSystemInfoSync()
+        console.log(phoneInfo)
+        this.phone = phoneInfo.model
+      } else {
+        this.phone = ''
+      }
+      // console.log(e)
     }
   },
   mounted () {
@@ -45,4 +100,10 @@ export default {
   padding: 10rpx;
   margin-top: 450rpx;
 }
+  .location{
+    margin-top: 10px;
+  }
+  .phone{
+    margin-top: 10px;
+  }
 </style>
